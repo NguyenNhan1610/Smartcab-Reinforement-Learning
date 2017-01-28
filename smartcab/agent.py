63c19e4160e320bf38a0d33c0731be993c2ff595
cgkaczenski <kzpulaski@gmail.com>
2017-01-28 14:23:11 -0600

    feat: Optimize the Q-learning agent
    - decay function change
    - reduce the state space
    - hyperparameter tuning

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -19,11 +19,8 @@ class LearningAgent(Agent):
         self.epsilon = epsilon   # Random exploration factor
         self.alpha = alpha       # Learning factor
 
-        ###########
-        ## TO DO ##
-        ###########
         # Set any additional class parameters as needed
-
+        self.num_trials = 0  	
 
     def reset(self, destination=None, testing=False):
         """ The reset function is called at the beginning of each trial.
@@ -33,13 +30,12 @@ class LearningAgent(Agent):
         # Select the destination as the new location to route to
         self.planner.route_to(destination)
         
-        ########### 
-        ## TO DO ##
-        ###########
         # Update epsilon using a decay function of your choice
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
-        self.epsilon = self.epsilon - 0.05
+        self.num_trials += 1
+        self.epsilon = math.exp(-0.1 * self.num_trials)
+        
 
         return None
 
@@ -53,8 +49,13 @@ class LearningAgent(Agent):
         inputs = self.env.sense(self)           # Visual input - intersection light and traffic
         deadline = self.env.get_deadline(self)  # Remaining deadline
         
-        # Set 'state' as a tuple of relevant data for the agent        
-        state = (waypoint, inputs['light'], inputs['oncoming'])
+        # Set 'state' as a tuple of relevant data for the agent
+        if inputs['oncoming'] == 'right' or inputs['oncoming'] == 'forward':
+        	left_turn = True
+        else:
+        	left_turn = False
+
+        state = (waypoint, inputs['light'], left_turn)
 
         return state
 
@@ -107,7 +108,7 @@ class LearningAgent(Agent):
         if random.random() < self.epsilon:
         	action = random.choice(self.valid_actions)
         else:
- 			action = self.get_maxQ(state)
+        	action = self.get_maxQ(state)
 
         return action
 
@@ -159,7 +160,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent, learning=True)
+    agent = env.create_agent(LearningAgent, learning=True, epsilon=0.8, alpha=0.8)
     
     ##############
     # Follow the driving agent
@@ -174,14 +175,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.01, display=False, log_metrics=True)
+    sim = Simulator(env, update_delay=0.01, display=False, log_metrics=True, optimized=True)
   
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10)
+    sim.run(tolerance=0.05, n_test=10)
 
 
 if __name__ == '__main__':
